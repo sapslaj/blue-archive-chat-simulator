cef51bc0857b6727882b3350a51f4f637ba67748
sapslaj <saps.laj@gmail.com>
2023-11-12 17:37:00 -0500

    s/class/className/

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -3,7 +3,7 @@ import { useCensorship } from "../hooks/useCensorship"
 export function Message(props: { message: string, placeholder?: string }) {
   const message = useCensorship(props.message || '');
   if (!props.message && props.placeholder) {
-    return <div className="message"><span class="message-placeholder">{props.placeholder}</span></div>
+    return <div className="message"><span className="message-placeholder">{props.placeholder}</span></div>
   }
   return <div className="message">{message}</div>
 }
