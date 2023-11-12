2e82b35936e423174f4e25221ee7c3b32d0728c1
sapslaj <saps.laj@gmail.com>
2023-11-12 17:35:38 -0500

    fix lint issue

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,9 +1,9 @@
 import { useCensorship } from "../hooks/useCensorship"
 
 export function Message(props: { message: string, placeholder?: string }) {
+  const message = useCensorship(props.message || '');
   if (!props.message && props.placeholder) {
     return <div className="message"><span class="message-placeholder">{props.placeholder}</span></div>
   }
-  const message = useCensorship(props.message);
   return <div className="message">{message}</div>
 }
